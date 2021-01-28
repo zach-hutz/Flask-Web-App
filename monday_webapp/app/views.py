b7daff40f5c7d9ecfbd36127c5e76b9124d5914e
zach-hutz <78053978+zach-hutz@users.noreply.github.com>
2021-01-28 10:11:46 -0600

    Add files via upload

--- monday_webapp/app/views.py
+++ monday_webapp/app/views.py
@@ -1,10 +1,25 @@
 from flask import render_template, request, redirect
 from app import app
 import os
+import csv
+import pandas as pd
 
-@app.route('/')
+
+@app.route('/', methods=["GET", "POST"])
 def index():
-    return render_template('index.html')
+    data = []
+    if request.method == 'POST':
+        if request.files:
+            uploaded_file = request.files['filename'] # This line uses the same variable and worked fine
+            filepath = os.path.join(app.config['FILE_UPLOADS'], uploaded_file.filename)
+            uploaded_file.save(filepath)
+            with open(filepath) as file:
+                csv_file = csv.reader(file)
+                for row in csv_file:
+                    data.append(row)
+            data = pd.DataFrame(data)
+            return render_template('index.html', data=data.to_html(header=False, index=False))
+    return render_template('index.html', data=data)
 
 
 @app.route('/help')
@@ -13,6 +28,7 @@ def help():
 
 app.config['FILE_UPLOADS'] = "C:\\Users\\Zachary\\Documents\\VSCode_Projects\\monday_webapp\\app\\static\\file\\uploads"
 
+'''
 @app.route('/upload', methods=['GET', 'POST'])
 def upload():
 
@@ -23,5 +39,5 @@ def upload():
             return redirect(request.url)
 
     return render_template('upload.html')
-
+'''
 
