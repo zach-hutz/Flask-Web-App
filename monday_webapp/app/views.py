b0047ccb3d131b70fc85c90863fb8bee630bee45
zach-hutz <78053978+zach-hutz@users.noreply.github.com>
2021-02-03 07:30:31 -0600

    Add files via upload

--- monday_webapp/app/views.py
+++ monday_webapp/app/views.py
@@ -4,6 +4,12 @@ import os
 import csv
 import pandas as pd
 from matplotlib import pyplot as plt
+import numpy as np
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
+import io
+import base64
+
 
 @app.route('/', methods=["GET", "POST"])
 def index():
@@ -17,9 +23,32 @@ def index():
                 csv_file = csv.reader(file)
                 for row in csv_file:
                     data.append(row)
-            data = pd.DataFrame(data)
+            data = pd.DataFrame(data[1:], columns=data[0])
+            if len(data.columns) >=3:
+                error_msg = 'Error please select a CSV with two columns'
+                return error_msg
 
-            return render_template('index.html', tables=[data.to_html(classes='data')], titles=data.columns.values, header=False, index=False)
+            d_list = list(data.columns.values)
+
+            x_label = d_list[0]
+            y_label = d_list[1]
+
+            new_file = uploaded_file.filename.replace(".csv", "")
+
+            fig = Figure()
+            axis = fig.add_subplot(1, 1, 1)
+            axis.set_title(new_file)
+            axis.set_xlabel(x_label)
+            axis.set_ylabel(y_label)
+            axis.grid()
+            axis.plot(data[x_label], data[y_label])
+
+            pngImage = io.BytesIO()
+            FigureCanvas(fig).print_png(pngImage)
+            pngImageB64String = "data:image/png;base64,"
+            pngImageB64String += base64.b64encode(pngImage.getvalue()).decode('utf8')
+
+            return render_template('index.html', image=pngImageB64String, tables=[data.to_html(classes='data')], titles=str(data.iloc[0]), header=False, index=False, index_names=False)
     return render_template('index.html', data=data)
 
 
@@ -28,19 +57,3 @@ def help():
     return render_template('help.html')
 
 app.config['FILE_UPLOADS'] = "C:\\Users\\Zachary\\Documents\\VSCode_Projects\\monday_webapp\\app\\static\\file\\uploads"
-
-'''
-@app.route('/upload', methods=['GET', 'POST'])
-def upload():
-
-    if request.method == 'POST':
-        if request.files:
-            uploaded_file = request.files['filename']
-            uploaded_file.save(os.path.join(app.config['FILE_UPLOADS'], uploaded_file.filename))
-            return redirect(request.url)
-
-    return render_template('upload.html')
-            a = [1,2,3,4]
-            string_a = json.dumps(a)
-'''
-
