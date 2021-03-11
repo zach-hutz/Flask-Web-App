4b5e4904fc9ed2f75b02ae187a7b3fbaf3d70b2e
zach-hutz <78053978+zach-hutz@users.noreply.github.com>
2021-03-11 17:25:59 -0600

    Add files via upload

--- monday_webapp/app/views.py
+++ monday_webapp/app/views.py
@@ -18,6 +18,7 @@ import json
 import sqlite3
 import stat
 import subprocess
+import time
 
 app.config['SECRET_KEY'] = 'Thisissupposedtobesecret!'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///C:/Users/Zachary/Documents/VSCode_Projects/monday_webapp/app/database.db'
@@ -57,15 +58,13 @@ def index():
         if request.files:
             uploaded_file = request.files['filename']
             filepath = os.path.join(app.config['FILE_UPLOADS'], uploaded_file.filename)
-            
+
             if current_user.get_id() is not None:
                 str_id = str(current_user.get_id())
                 usr_fp = os.path.join("C:\\Users\\Zachary\\Documents\\VSCode_Projects\\monday_webapp\\app\\static\\user_data\\" + str_id, uploaded_file.filename)
                 uploaded_file.save(usr_fp)
-                print("if met")
             else:
                 uploaded_file.save(filepath)
-                print("else met")
 
             if current_user.get_id() is None:
                 with open(filepath) as file:
@@ -78,6 +77,7 @@ def index():
                     csv_file = csv.reader(file)
                     for row in csv_file:
                         data.append(row)
+
             data = pd.DataFrame(data[1:], columns=data[0])
             d_list = list(data.columns.values)
 
@@ -96,28 +96,16 @@ def index():
             graph_label.append(y_label)
             graph_label = [i.replace('"', '') for i in graph_label]
             graph_label = [i.replace('"', '') for i in graph_label]
-            print("GRAPH_LABEL - " + str(graph_label))   
 
             ydump = json.dumps(data_y_array)
-            print("YDUMP - " + str(ydump))
-            print("X_LABEL - " + x_label)
-            print("Y LABEL - " + y_label)
-            
-            
-            print(data_x_array)
             xdump = json.dumps(data_x_array)
-            print(xdump)
-            print(xdump)
-
 
             data_arrays = []
             for col in d_list:
                 data_arrays.append(list(data[col]))
             data_arrays = data_arrays[1:]
             json_data = json.dumps(data_arrays)
-            print(str(json_data))
 
-            print(d_list)
             colname = json.dumps(d_list)
 
             horiz_data = pd.DataFrame(data_arrays)
@@ -125,11 +113,10 @@ def index():
             for i in range(0, len(horiz_data.iloc[:, 0])):
                 horiz_array.append(list(horiz_data.iloc[:, i]))
 
-            print(horiz_array)
             horiz_array_dumps = json.dumps(horiz_array)
 
 
-            return render_template('index.html', horiz_array=horiz_array_dumps, columname=colname, json_data=json_data, graphlabel=graph_label, y_label=y_label, datayarray=ydump, dataxarray=xdump, chart_name=file_name, tables=[data.to_html(classes='data')], titles=str(data.iloc[0]), header=False, index=False, index_names=False)
+            return render_template('index.html', name=current_user.username, horiz_array=horiz_array_dumps, columname=colname, json_data=json_data, graphlabel=graph_label, y_label=y_label, datayarray=ydump, dataxarray=xdump, chart_name=file_name, tables=[data.to_html(classes='data')], titles=str(data.iloc[0]), header=False, index=False, index_names=False)
     return render_template('index.html', data=data)
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -145,7 +132,7 @@ def login():
                 if os.path.exists(os.path.join(os.path.dirname(absolute_path) + "\\static\\user_data", str_id)) == False:
                     os.mkdir(os.path.join(os.path.dirname(absolute_path) + "\\static\\user_data", str_id), mode=0o777)
                     os.chmod(os.path.join(os.path.dirname(absolute_path) + "\\static\\user_data", str_id), stat.S_IWRITE)
-                return redirect('/dashboard')
+                return redirect('/')
 
         return '<h1>Invalid username or password</h1>'
     return render_template('login.html', form=form)
@@ -160,25 +147,52 @@ def signup():
         db.session.add(new_user)
         db.session.commit()
 
-        return '<h1>New user has been created!</h1>'
+        return render_template("index.html")
 
     return render_template('signup.html', form=form)
 
 @app.route('/dashboard')
 @login_required
 def dashboard():
-    return render_template('dashboard.html', name=current_user.username)
+    files_in_dir = 0
+    user_id = "0"
+    user_id_list = list(user_id)
+    user_id_list_dump = json.dumps(user_id_list)
+    
+    files_in_dir_dump = json.dumps(files_in_dir)
+    file_names_in_dir_dump = json.dumps(files_in_dir)
+
+    if current_user.get_id() is not None:
+        user_id = str(current_user.get_id())
+        user_id_list = list(user_id)
+        user_id_list_dump = json.dumps(user_id_list)
+        print(user_id_list_dump)
+        str_id = str(current_user.get_id())
+        directory = os.path.join("C:\\Users\\Zachary\\Documents\\VSCode_Projects\\monday_webapp\\app\\static\\user_data\\",str_id)
+
+
+        files_in_dir = next(os.walk(directory))[2]
+        files_in_dir_dump = json.dumps(len(files_in_dir))
+        file_names_in_dir_dump = json.dumps(files_in_dir)
+        print(file_names_in_dir_dump)
+        print(files_in_dir_dump)
+
+    files_in_dir_dump = json.dumps(len(files_in_dir))
+    file_names_in_dir_dump = json.dumps(files_in_dir)
+    print(file_names_in_dir_dump)
+    print(files_in_dir_dump)
+
+    return render_template('dashboard.html', name=current_user.username, dir_names=file_names_in_dir_dump, dir_length=files_in_dir_dump)
 
 @app.route('/logout')
 @login_required
 def logout():
     logout_user()
-    return redirect('/')
+    return render_template("logout.html")
 
 @app.route('/help')
 def help():
     return render_template('help.html')
-
 
 
 app.config['FILE_UPLOADS'] = "C:\\Users\\Zachary\\Documents\\VSCode_Projects\\monday_webapp\\app\\static\\file\\uploads"
